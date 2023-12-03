35d94801ec0b6c0358971ee87ea5389a3301d850
mimu <primaryscholar@126.com>
2023-12-03 15:32:59 +0800

    add a project

--- simple-a-pom/src/test/java/com/mimu/simple/SpringTest.java
+++ simple-a-pom/src/test/java/com/mimu/simple/SpringTest.java
@@ -2,10 +2,15 @@ package com.mimu.simple;
 
 
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringTest {
 
     @Test
     public void Test() {
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+        for (String s : configApplicationContext.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
     }
 }
